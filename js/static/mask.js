6ece37d7bc093e34c505ea0e7932b513aed5b7e6
luojinghui <939035243@qq.com>
2016-05-05 17:35:31 +0800

    add mask dialog function

---
+++ js/static/mask.js
@@ -0,0 +1,18 @@
+/**
+ * Created by: Luojinghui/luojinghui424@gmail.com
+ * Date: 16/5/4
+ * Time: 下午4:28
+ */
+define(function(require, exports, module){
+    var $ = require('jquery');
+
+    function Mask() {
+
+    }
+
+    Mask.prototype.createTitle = function() {
+
+    };
+
+    module.exports = Mask;
+});
